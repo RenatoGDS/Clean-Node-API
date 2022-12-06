7920486330fe6c79d979c01e767fa3a5435fc95c
Renato Gomes <renato_kusanagi@hotmail.com>
2022-12-06 12:31:32 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,26 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
-  moduleNameMapper: {
-    '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1'
-  },
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/tests'
-  ],
+  testEnvironment: 'node',
   transform: {
     '\\.ts$': 'ts-jest'
   }
 }
+
+// module.exports = {
+//  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+// coverageDirectory: 'coverage',
+// coverageProvider: 'babel',
+// moduleNameMapper: {
+// '@/tests/(.+)': '<rootDir>/tests/$1',
+// '@/(.+)': '<rootDir>/src/$1'
+// },
+// roots: [
+// '<rootDir>/src',
+// '<rootDir>/tests'
+// ],
+// transform: {
+// '\\.ts$': 'ts-jest'
+// }
+// }
